d5117b07381f4b74d3eb8d06bbcb457e71638c5d
Zulfikar_ST <148250347+zuliorr@users.noreply.github.com>
2026-05-04 02:16:37 +0800

    Add files via upload

---
+++ meet3.pyz.py
@@ -0,0 +1,23 @@
+# minta user memasukkan angka.
+# jika angka negatif,minta user memasukkan angka positif
+
+# contoh:
+# masukkan angka : -3
+# harus positif!
+# masukkan angka:
+
+# jawab:
+
+
+while True:
+    angka =int(input("masukan angka yang anda mau"))
+    
+    if angka >= 0:
+        print (f"niali angka:{angka}")
+        print ("allahumasalialasayidinamuhammadwaalasiyidinamuhammad")
+
+    else:
+         print("harus positif!")
+         print("silahkan masukan angka yg positif di hlaman berikut")
+    
+    print (f"terimakasih anda memasukan angka{angka}")
