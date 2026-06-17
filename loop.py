e39182becf9b66abc8034ba6d69814f93e76fbf6
Zulfikar_ST <148250347+zuliorr@users.noreply.github.com>
2026-06-17 20:22:12 +0800

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,13 @@
+angka = [0,1,2,3,4] # ini menggunakan list 
+
+for i in angka :
+    print(f"nilai i = {i}")
+    print("pak edy ganteng")
+print("program 1 selesai selesai \n")
+
+angka = range(2,9) #ini menggunakan range
+
+for i in angka :
+    print(f"nilai i = {i}")
+    print("pak edy ganteng")
+print("program selesai")
