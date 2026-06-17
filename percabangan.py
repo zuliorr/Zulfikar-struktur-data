958d27be9f6ac602f5ee64b8cb20a4ab6534c647
Zulfikar_ST <148250347+zuliorr@users.noreply.github.com>
2026-06-17 20:48:43 +0800

    Implement basic conditional greetings in percabangan.py

---
+++ percabangan.py
@@ -0,0 +1,17 @@
+# percabangan
+# 1. if nya
+# 2. kondisi
+# 3. aksi
+
+nama = input("masukan nama anda :")
+if nama == "edy" : # kondisi pertama
+    print("selamat datang edy") # aksi true pertama
+    print("Dosen ganteng")
+elif nama=="syhasdik" : # kondisi kedua
+    print("selamat datang syhasdik") # aksi true kedua
+elif nama=="bunga" : # kondisi ketiga
+    print("selamat datang bunga") # aksi true ketiga
+else :
+    print("anda bukan dosen")
+    print("anda tidak ganteng")
+print("program selesai")
